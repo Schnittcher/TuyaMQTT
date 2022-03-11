4fb1e83524fb843b5adf49419c4692fd761ad942
Kai Schnittcher <kai.schnittcher@gmail.com>
2022-03-11 10:46:50 +0100

    fix style

--- SimpleSwitch/module.php
+++ SimpleSwitch/module.php
@@ -1,23 +1,23 @@
 <?php
 
 declare(strict_types=1);
-	class SimpleSwitch extends IPSModule
-	{
-		public function Create()
-		{
-			//Never delete this line!
-			parent::Create();
-		}
+    class SimpleSwitch extends IPSModule
+    {
+        public function Create()
+        {
+            //Never delete this line!
+            parent::Create();
+        }
 
-		public function Destroy()
-		{
-			//Never delete this line!
-			parent::Destroy();
-		}
+        public function Destroy()
+        {
+            //Never delete this line!
+            parent::Destroy();
+        }
 
-		public function ApplyChanges()
-		{
-			//Never delete this line!
-			parent::ApplyChanges();
-		}
-	}
+        public function ApplyChanges()
+        {
+            //Never delete this line!
+            parent::ApplyChanges();
+        }
+    }
