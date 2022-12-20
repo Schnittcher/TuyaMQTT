0b9c0729e7b0c4b780e6b1bd7254bdb486d87aef
Kai Schnittcher <kai.schnittcher@gmail.com>
2022-12-20 14:37:05 +0100

    Fix: MQTT UTF8 Fix

--- RGBTWLight/module.php
+++ RGBTWLight/module.php
@@ -110,6 +110,11 @@ eval('declare(strict_types=1);namespace TuyaMQTT {?>' . file_get_contents(__DIR_
             if (!empty($this->ReadPropertyString('MQTTTopic'))) {
                 $Buffer = json_decode($JSONString, true);
 
+                //Für MQTT Fix in IPS Version 6.3
+                if (IPS_GetKernelDate() > 1670886000) {
+                    $Buffer['Payload'] = utf8_decode($Buffer['Payload']);
+                }
+
                 $this->SendDebug('ReceiveData :: Buffer', $Buffer, 0);
 
                 if (array_key_exists('Topic', $Buffer)) {
